5e91dc9a08d4f748b21c9c45d49dd6149ffb6e16
Pedro Cavaleiro <pm.cavaleiro@hotmail.com>
2018-11-12 01:32:47 +0000

    Reworked TrustAgent project (updated network packet build)

---
+++ TrustAgent/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Net;
+using System.Linq;
+using System.Net.Sockets;
+
+using static TrustAgent.StandardPrints;
+using static TrustAgent.Helpers;
+using static TrustAgent.AESCipher;
+using System.Text;
+
+namespace TrustAgent
+{
+    class Program
+    {
+
+        #region "Constants"
+
+        //Used when there is no need to use a key or it's not possible to use one,
+        //This key IS NOT to build a secure/valid HMAC but rather for the system
+        //to be able to generate an hmac as all functions require
+        public const string DEFAULT_KEY = "a77acc4e2b2586f8b58795573c5227661f93877abcc3878ba6c6179022e10f4e";
+
+        #endregion
+
+        #region "Variables"
+
+        public static bool enableDebug;
+        public static bool enableSpy;
+        static int serverPort = 12345;
+
+        static int dbSeed1;
+        static int dbSeed2;
+        static string dbPath;
+
+        static string commandPrompt;
+        static string computerName;
+
+        public static Database database;
+        public static Server server;
+        static MenuManager menuManager;
+        public static Client spy;
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            ProcessLog(ProcessPrint.Info, "TrustAgent is initializing...");
+            dbPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/tadb.bin";
+
+            if (args.Length > 0)
+                ProcessArgs(args);
+            else {
+                Seed1Validation();
+                Seed2Validation();
+            }
+
+            List<IPAddress> ips = GetLocalIPAddress();
+            computerName = Dns.GetHostName().Replace(".local", "").Replace(".lan", "");
+            commandPrompt = string.Format("TrustAgent ({0}) > ", computerName);
+
+            ProcessDebugMessage(string.Format("Computer name: {0} > ", computerName));
+            ProcessDebugMessage(string.Format("Fetchig available IP's: {0} > ", computerName));
+
+            if (ips.Count <= 0)
+            {
+                ProcessLog(ProcessPrint.Critical, "There are no available networks!");
+                Environment.Exit(0);
+            }
+
+            foreach (IPAddress ip in ips)
+                ProcessDebugMessage(ip.ToString());
+
+            database = new Database(dbSeed1, dbSeed2, dbPath);
+
+            if (File.Exists(dbPath)) {
+                if (!ValidateDecryption(database.entities, 
+                                       GenKey(dbSeed1, database.selector),
+                                       GenIV(dbSeed2, database.selector))) {
+                    File.Delete(dbPath);
+                    ProcessLog(ProcessPrint.Error, "Incorrect seeds! Database deleted to prevent bruteforce!");
+                    database.entities = null;
+                    database.selector = 0;
+                    database.fileExists = false;
+                }
+            }
+
+            Console.WriteLine("");
+            ProcessLog(ProcessPrint.Info, "Type help at anytime to show the help menu\n");
+
+            server = new Server(ips.First(), serverPort);
+            server.ClientConnected += Server_ClientConnected;
+            server.MessageReceived += Server_MessageReceived;
+
+            if (enableSpy) {
+                //To connect for the first time to the spy
+                byte[] connectedString = Encoding.ASCII.GetBytes("f");
+                byte[] size = BitConverter.GetBytes(connectedString.Length);
+                byte[] packet = new byte[connectedString.Length + 4];
+                Array.Copy(size, 0, packet, 0, 4);
+                Array.Copy(connectedString, 0, packet, 4, connectedString.Length);
+                spy = new Client();
+                spy.Connect(ips.First().ToString(), 11223, packet);
+            }
+
+            menuManager = new MenuManager();
+
+        }
+
+        static void Server_MessageReceived(byte[] m, ClientHandler clientHandler)
+        {
+        }
+
+        static void Server_ClientConnected(byte[] m, TcpClient socket, EventArgs e)
+        {
+            IPAddress ip = ((IPEndPoint)socket.Client.RemoteEndPoint).Address;
+            IPAddress local = ((IPEndPoint)socket.Client.LocalEndPoint).Address;
+
+            Server.DeconstructPacket(m, out byte[] hmac, out ClientMessage message);
+            if (enableDebug)
+                ReplaceWith("");
+            ProcessDebugMessage(string.Format("New entity is trying to connect: {0} ({1})", message.Entity, ip));
+
+            Database.ValidationError validation = database.ValidateEntity(message, hmac, m);
+            switch (validation)
+            {
+                case Database.ValidationError.NotFound:
+                    server.DennyClient(message.Entity, ServerMessages.EntityNotFound, socket, DEFAULT_KEY.FromHexToByteArray());
+                    ProcessDebugMessage(string.Format("Connection from entity {0} ({1}) refused! Entity not found!", message.Entity, ip));
+                    break;
+                case Database.ValidationError.InvalidKey:
+                    server.DennyClient(message.Entity, ServerMessages.InvalidKey, socket, DEFAULT_KEY.FromHexToByteArray());
+                    ProcessDebugMessage(string.Format("Connection from entity {0} ({1}) refused! Invalid key or message tempered!", message.Entity, ip));
+                    break;
+                case Database.ValidationError.AlreadyConnected:
+                    server.DennyClient(message.Entity, ServerMessages.EntityAlreadyConnected, socket, DEFAULT_KEY.FromHexToByteArray());
+                    ProcessDebugMessage(string.Format("Connection from entity {0} ({1}) refused! Entity already connected!", message.Entity, ip));
+                    break;
+                case Database.ValidationError.NoError:
+                    ProcessDebugMessage(string.Format("Connection from entity {0} ({1}) accepted!", message.Entity, ip));
+                    server.AcceptClient(message.Entity, socket, database.RetreiveEntityKey(message.Entity), enableSpy, local.ToString());
+                    break;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("\r" + menuManager.commandPrefix);
+        }
+
+
+        #region "Initialization Helpers"
+
+        static void ProcessArgs(string[] args) {
+            enableSpy |= args.Contains("-spy");
+            enableDebug |= args.Contains("-debug");
+
+            ProcessDebugMessage("TrustAgent is using AES-256-CBC as cipher");
+            ProcessDebugMessage("TrustAgent is using SHA-256 to build HMACs");
+
+            ProcessDebugMessage("Debug mode is enabled");
+            if (enableSpy)
+                ProcessDebugMessage("SPY Mode is enabled");
+
+            if (args.Contains("-port"))
+            {
+                int pos = Array.IndexOf(args, "-port");
+                bool success = int.TryParse(args[pos + 1], out serverPort);
+                if (!success)
+                    ProcessLog(ProcessPrint.Warn, "Unable to parse the port, falling to default!");
+            }
+            else
+                ProcessDebugMessage("Port not detected, falling to default!");
+
+            if (args.Contains("-seed1")) {
+                int pos = Array.IndexOf(args, "-seed1");
+                bool success = int.TryParse(args[pos + 1], out dbSeed1);
+                if (!success) {
+                    Seed1Validation();
+                }
+            } else {
+                Seed1Validation();
+            }
+
+            if (args.Contains("-seed2"))
+            {
+                int pos = Array.IndexOf(args, "-seed2");
+                bool success = int.TryParse(args[pos + 1], out dbSeed2);
+                if (!success)
+                {
+                    Seed2Validation();
+                }
+            }
+            else
+            {
+                Seed2Validation();
+            }
+
+        }
+
+        static void Seed1Validation() {
+            if (File.Exists(dbPath))
+            {
+                ProcessLog(ProcessPrint.Error, "Unable to parse seed 1! Further execution will result in data loss!");
+                ProcessLog(ProcessPrint.Question, "Do you wish to continue (y/n)");
+                if (Console.ReadLine().ToBool())
+                {
+                    dbSeed1 = GenerateSeed();
+                    ProcessLog(ProcessPrint.Info, "New seed was generated. Check the system menu!");
+                }
+                else
+                {
+                    ProcessLog(ProcessPrint.Critical, "Server initialization stopped by user");
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        static void Seed2Validation()
+        {
+            if (File.Exists(dbPath))
+            {
+                ProcessLog(ProcessPrint.Error, "Unable to parse seed 2! Further execution will result in data loss!");
+                ProcessLog(ProcessPrint.Question, "Do you wish to continue (y/n)");
+                if (Console.ReadLine().ToBool())
+                {
+                    dbSeed2 = GenerateSeed();
+                    ProcessLog(ProcessPrint.Info, "New seed was generated. Check the system menu!");
+                }
+                else
+                {
+                    ProcessLog(ProcessPrint.Critical, "Server initialization stopped by user");
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+
+
+        #endregion
+
+    }
+}
---
+++ TrustAgent/Server.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+
+using static TrustAgent.SHA256hmac;
+
+namespace TrustAgent
+{
+    public class Server
+    {
+
+        #region "Variables"
+
+        Thread thread;
+        bool stop;
+
+        public EventArgs e;
+
+        public Hashtable clientHandlers = new Hashtable();
+
+        #endregion
+
+        #region "Events"
+
+        public event ClientCommunicationHandler MessageReceived;
+        public event ClientConnectionHandler ClientConnected;
+
+        #endregion
+
+        #region "Delegates"
+
+        public delegate void ClientCommunicationHandler(byte[] m, ClientHandler clientHandler);
+        public delegate void ClientConnectionHandler(byte[] m, TcpClient socket, EventArgs e);
+
+        #endregion
+
+        public Server(IPAddress ip, int port)
+        {
+            thread = new Thread(() => StartListening(ip, port));
+            thread.Start();
+        }
+
+        void StartListening(IPAddress ip, int port)
+        {
+            TcpListener serverSocket = new TcpListener(ip, port);
+            TcpClient clientSocket = default(TcpClient);
+
+            int counter = 0;
+            serverSocket.Start();
+
+            while (!stop)
+            {
+                if (serverSocket.Pending())
+                {
+                    counter += 1;
+                    clientSocket = serverSocket.AcceptTcpClient();
+
+
+                    byte[] dataLength = new byte[4];
+                    NetworkStream stream = clientSocket.GetStream();
+                    stream.Read(dataLength, 0, 4);
+
+                    byte[] packet = new byte[BitConverter.ToInt32(dataLength) + 4];
+                    stream.Read(packet, 0, BitConverter.ToInt32(dataLength) + 4);
+
+                    byte[] data = new byte[BitConverter.ToInt32(dataLength)];
+                    Array.Copy(packet, 4, data, 0, BitConverter.ToInt32(dataLength));
+
+                    ClientConnected(data, clientSocket, e);
+
+                }
+            }
+
+            BroadcastShutdownToClients();
+            if (clientSocket != null)
+                clientSocket.Close();
+            serverSocket.Stop();
+            foreach (DictionaryEntry item in clientHandlers)
+                ((ClientHandler)item.Value).Disconnect();
+        }
+
+        /// <summary>
+        /// Accepts a client a saves all the data in a Hashtable
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        /// <param name="socket">Socket.</param>
+        /// <param name="key">Key.</param>
+        /// <param name="enableSpy">If set to <c>true</c> enable spy.</param>
+        /// <param name="spyIP">Spy ip.</param>
+        /// <param name="spyPort">Spy port.</param>
+        public void AcceptClient(string entity, TcpClient socket, byte[] key, bool enableSpy = false, string spyIP = "", int spyPort = 11223)
+        {
+
+            SendMessage(new ServerCommand { Command = ServerOperations.Connected.Value, EnableSpy = enableSpy, SpyIP = spyIP, SpyPort = spyPort },
+                        PacketType.ServerCommand, key, socket);
+
+            ClientHandler client = new ClientHandler(entity, socket);
+            client.MessageReceived += Client_MessageReceived;
+
+            clientHandlers.Add(entity, client);
+        }
+
+        public void DennyClient(string entity, ServerMessages message, TcpClient socket, byte[] key) {
+            SendMessage(new ServerCommand { Command = ServerOperations.ConnectionRefused.Value, Message = message.Value },
+                        PacketType.ServerCommand, key, socket);
+        }
+
+        /// <summary>
+        /// Handles a message received from a client
+        /// </summary>
+        /// <param name="handler">Handler.</param>
+        /// <param name="m">M.</param>
+        void Client_MessageReceived(ClientHandler handler, byte[] m)
+        {
+            MessageReceived(m, handler);
+        }
+
+        /// <summary>
+        /// Sends a message to a specific client (socket)
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="type">Type.</param>
+        /// <param name="key">Key.</param>
+        /// <param name="socket">Socket.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public void SendMessage<T>(T message, PacketType type, byte[] key, TcpClient socket)
+        {
+            new Thread(delegate ()
+            {
+                NetworkStream stream = socket.GetStream();
+                byte[] packet = BuildPacket(message, key, type);
+                stream.Write(packet, 0, packet.Length);
+                stream.Flush();
+                if (Program.enableSpy)
+                    Program.spy.SendPacket(packet);
+            }).Start();
+        }
+
+        /// <summary>
+        /// Sends a signal to all connected clients that the server is shutting down
+        /// </summary>
+        public void BroadcastShutdownToClients() {
+            int threads = clientHandlers.Count;
+            CountdownEvent countdown = new CountdownEvent(threads);
+
+            foreach (DictionaryEntry item in clientHandlers) {
+                ClientHandler handler = (ClientHandler)item.Value; 
+                string entity = handler.Entity;
+                byte[] packet = BuildPacket(new ServerCommand { Command = ServerOperations.ServerShutdown.Value },
+                                            Program.database.RetreiveEntityKey(entity), PacketType.ServerCommand);
+                new Thread(delegate ()
+                {
+                    NetworkStream stream = handler.Socket.GetStream();
+                    stream.Write(packet, 0, packet.Length);
+                    stream.Flush();
+                    if (Program.enableSpy)
+                        Program.spy.SendPacket(packet);
+                    countdown.Signal();
+                }).Start();
+            }
+            countdown.Wait();
+        }
+
+        /// <summary>
+        /// Initializes the shutdown process
+        /// </summary>
+        public void Shutdown() { stop = true; }
+
+        #region "Network Helpers"
+
+        /// <summary>
+        /// Builds a packet to be sent to the client
+        /// A packet follows the following diagram
+        /// +-----------------------------------------------------------+
+        /// | PACKET SIZE | PACKET TYPE | HMAC (SHA256) |    MESSAGE    |
+        /// |    INT 32   |    INT 32   |  BYTE ARRAY   |    STRING     |
+        /// |   4 BYTES   |   4 BYTES   |   32 BYTES    | VARIABLE SIZE |
+        /// +-----------------------------------------------------------+
+        /// </summary>
+        /// <returns>The packet.</returns>
+        /// <param name="message">Message.</param>
+        /// <param name="key">Key.</param>
+        /// <param name="type">Type.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static byte[] BuildPacket<T>(T message, byte[] key, PacketType type)
+        {
+            string json = JsonConvert.SerializeObject(message);
+            byte[] packetType = BitConverter.GetBytes(type.Value);
+            byte[] data = Encoding.ASCII.GetBytes(json);
+            byte[] hmac = ComputeHMAC(data, key);
+            byte[] size = BitConverter.GetBytes(hmac.Length + data.Length);
+            byte[] packet = new byte[data.Length + hmac.Length + 4];
+            Array.Copy(size, packet, 4);
+            Array.Copy(packetType, 0, packet, 4, 4);
+            Array.Copy(hmac, 0, packet, 8, hmac.Length);
+            Array.Copy(data, 0, packet, 8 + hmac.Length, data.Length);
+            return packet;
+        }
+
+        /// <summary>
+        /// Deconstructs the packet.
+        /// </summary>
+        /// <param name="packet">Packet.</param>
+        /// <param name="hmac">Hmac.</param>
+        /// <param name="message">Message.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static void DeconstructPacket<T>(byte[] packet, out byte[] hmac, out T message)
+        {
+            hmac = new byte[32];
+            message = default(T);
+
+            Array.Copy(packet, 4, hmac, 0, 32);
+            byte[] msg = new byte[packet.Length - 36];
+            Array.Copy(packet, 36, msg, 0, packet.Length - 36);
+            message = JsonConvert.DeserializeObject<T>(Encoding.ASCII.GetString(msg));
+        }
+
+        /// <summary>
+        /// Decodes the type of the packet.
+        /// </summary>
+        /// <returns>The packet type.</returns>
+        /// <param name="packet">Packet.</param>
+        public static PacketType DecodePacketType(byte[] packet)
+        {
+            byte[] packetType = new byte[4];
+            Array.Copy(packet, packetType, 4);
+            return new PacketType(BitConverter.ToInt32(packetType));
+        }
+
+        #endregion
+
+    }
+}
