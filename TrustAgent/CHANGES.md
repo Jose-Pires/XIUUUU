3bd02f15574066f50642186927e85662af5a053a
Pedro Cavaleiro <pm.cavaleiro@hotmail.com>
2018-12-12 02:10:03 +0000

    Cleared repo

--- TrustAgent/CHANGES.md
+++
@@ -1,9 +0,0 @@
-﻿# TrustAgent Changes
-
-* Fixed verification of entity already connected error upon message received from said client
-* ClientHandler message decoder was skiping 4 bytes leading to missing bytes on the string
-* Removed unecessary coments
-* Comand from client "disconnect" is now recognized by the server
-* Comand from client "request_key_negotiation" is now recognized by the server (everything was implemented except the comand recognizer for this comand)
-
-TrustAgent Released as 1.0.0
--- TrustAgent/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0017:Comparação possível do tipo de valor com 'null'", Justification = "<Pendente>", Scope = "member", Target = "~M:TrustAgent.Helpers.ToByteArray``1(``0)~System.Byte[]")]
-
